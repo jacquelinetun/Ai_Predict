d4d466fc1864eecf6e5fce50a268a2117ab8919d
jacquelinetun <jacquelinenaychi@gmail.com>
2023-08-18 09:42:20 +0630

    ai predict files updated

--- Model.py
+++ Model.py
@@ -9,7 +9,7 @@ def main():
     st.title("Income Prediction App")
     st.write("Enter your age and years of experience to predict income.")
 
-    age = st.slider("Age", 18, 100, 25)
+    age = st.slider("Age", 18, 70, 25)
     experience = st.slider("Experience (years)", 0, 50, 5)
 
     if st.button("Predict Income"):
