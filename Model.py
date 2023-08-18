5f1ead8c59f07cf36045968753b5847e6c06ca68
jacquelinetun <jacquelinenaychi@gmail.com>
2023-08-18 09:20:16 +0630

    ai predict files commit

---
+++ Model.py
@@ -0,0 +1,21 @@
+import joblib
+
+import streamlit as st
+
+# Load the model
+loaded_model = joblib.load('model.pkl')
+
+def main():
+    st.title("Income Prediction App")
+    st.write("Enter your age and years of experience to predict income.")
+
+    age = st.slider("Age", 18, 100, 25)
+    experience = st.slider("Experience (years)", 0, 50, 5)
+
+    if st.button("Predict Income"):
+        input_data = [[age, experience]]
+        predicted_income = loaded_model.predict(input_data)[0]
+        st.success(f"Predicted Income: ${predicted_income:.2f}")
+
+if __name__ == "__main__":
+    main()
